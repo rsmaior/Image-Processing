329060b782e1148464b5873cd3350b2aea0c84a9
rsmaior <iquequeisso@gmail.com>
2017-08-19 20:49:24 -0300

    Update otimizar_imagens.sh

--- otimizar_imagens.sh
+++ otimizar_imagens.sh
@@ -1,17 +1,29 @@
 #/bin/bash
 
 
+##### Geotiff RGB ########
+for f in *;
+do
+gdal_translate       \
+--config GDAL_NUM_THREADS ALL_CPUS   -co PHOTOMETRIC=YCBCR  -co COMPRESS=JPEG   -co TILED=YES   -b 1 -b 2 -b 3       \                   \
+$f o_$f                                \
+&& \
+gdaladdo       \
+-ro -r average --config COMPRESS_OVERVIEW JPEG --config PHOTOMETRIC_OVERVIEW YCBCR --config INTERLEAVE_OVERVIEW PIXEL \
+o_$f  2 4 8 16 32 64;
+done;
+#############################
 
-for f in *; do
 
-### reproject from WGS84 to SAD69 UTM 23S
-#gdalwarp -overwrite -s_srs EPSG:4326 -t_srs EPSG:29193 -of GTiff    mosaico      2172_4_SE_SAD69.tif
-
-
-gdal_translate   -co PHOTOMETRIC=YCBCR  -co COMPRESS=JPEG   -co TILED=YES  -b 1 -b 2 -b 3  $f o_$f
-
-gdaladdo -ro -r average --config COMPRESS_OVERVIEW JPEG --config PHOTOMETRIC_OVERVIEW YCBCR --config INTERLEAVE_OVERVIEW PIXEL o_$f  2 4 8 16 32 64
-
-  
-done
-
+##### ERDAS IMG #######
+for f in * ;
+do 
+gdal_translate \
+-stats  -scale  -co PHOTOMETRIC=YCBCR  -co COMPRESS=JPEG  -co TILED=YES  -ot Byte  -of GTiff  -b 1 -b 2 -b 3  \
+--config GDAL_NUM_THREADS ALL_CPUS    $f    O_${f%.img}.tif \ 
+&& \
+gdaladdo \
+-ro -r average --config COMPRESS_OVERVIEW JPEG --config PHOTOMETRIC_OVERVIEW YCBCR --config INTERLEAVE_OVERVIEW PIXEL   \
+O_${f%.img}.tif    2 4 8 16 32 64;
+done;
+##########################
