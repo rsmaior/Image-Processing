f96303ae32057cf497cdcffbc2871097e760f320
rsmaior <iquequeisso@gmail.com>
2018-03-15 14:27:02 -0300

    Update otimizar_imagens.sh

--- otimizar_imagens.sh
+++ otimizar_imagens.sh
@@ -6,7 +6,7 @@
 ##### addo: cria pirâmides pela média e comprime cada uma
 for f in *;
 do gdal_translate \
---config GDAL_NUM_THREADS ALL_CPUS   -co PHOTOMETRIC=YCBCR  -co COMPRESS=JPEG   -co TILED=YES   -b 1 -b 2 -b 3 \                   \
+--config GDAL_NUM_THREADS ALL_CPUS   -co PHOTOMETRIC=YCBCR  -co COMPRESS=JPEG   -co TILED=YES   -b 1 -b 2 -b 3 \
 $f o_$f   \
 && \
 gdaladdo \
@@ -21,7 +21,7 @@ done;
 ##### addo: cria pirâmides pela média e comprime cada uma
 for f in * ;
 do gdal_translate \
--b 1 -b 2 -b 3    -of GTiff  -ot Byte -scale    -co PHOTOMETRIC=YCBCR -co COMPRESS=JPEG -co TILED=YES      \
+-b 1 -b 2 -b 3    -of GTiff  -ot Byte -scale    -co PHOTOMETRIC=YCBCR -co COMPRESS=JPEG -co TILED=YES \
 --config GDAL_NUM_THREADS ALL_CPUS    $f    O_${f%.img}.tif \
 && \
 gdaladdo \
@@ -33,6 +33,7 @@ done;
 
 ##### Silvania ####
 for f in * ;
-time gdalwarp -overwrite -s_srs EPSG:32722 -t_srs EPSG:4674 -r near -co NUM_THREADS=ALL_CPUS --config GDAL_CACHEMAX 1000 \
+do gdalwarp \
+-s_srs EPSG:32722 -t_srs EPSG:4674 -r near -co NUM_THREADS=ALL_CPUS --config GDAL_CACHEMAX 1000 \
 -wm 1000 -multi -q -cutline MOLDURA.shp -dstalpha -of GTiff o_$f $f_SIRGAS2000.tif;
 done;
